58a784f3140cca9ce26c1aec971a25fb1a44ddc9
Miguel Caballero <miguelcabgil@gmail.com>
2023-01-27 15:44:25 -0500

    refactor(auth-service): remove microservice implementation from main.ts

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,7 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { EnvConfigService } from './config/env-config/env-config.service';
 import supertokens from 'supertokens-node';
 import { SupertokensFilter } from './common/filters/supertokens.filter';
@@ -15,27 +14,5 @@ import { SupertokensFilter } from './common/filters/supertokens.filter';
     credentials: true,
   });
   app.useGlobalFilters(new SupertokensFilter());
-  const microservice = app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [
-        {
-          port: config.getRabbitmqPort(),
-          hostname: config.getRabbitmqHostname(),
-          password: config.getRabbitmqPassword(),
-          username: config.getRabbitmqUsername(),
-        },
-      ],
-      queue: config.getRabbitmqQueue(),
-      queueOptions: {
-        durable: true,
-      },
-      noAck: false,
-      prefetchCount: 1,
-    },
-  });
-  await Promise.all([
-    app.startAllMicroservices(),
-    app.listen(process.env.PORT ?? 7777),
-  ]);
+  await app.listen(process.env.PORT ?? 7777);
 })().catch((e) => Logger.error(e));
